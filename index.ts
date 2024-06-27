16e390ceecbc7b0ad145214bc63cf0b906a824ab
Noor-ul-Sehar <160354793+nh7220869@users.noreply.github.com>
2024-06-27 16:20:32 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: chalk.yellowBright.bold("Please enter the number of second"),
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please enter valid number"
+        }else if (input > 60) {
+            return "seconds must be in 60"
+
+        }else{
+            return true;
+        }
+    }
+});
+
+let input = res.userInput
+
+function startTime(val:number){
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+  setInterval((() => {
+    const currentTime = new Date()
+    const timeDiff = differenceInSeconds(intervalTime, currentTime);
+    if (timeDiff <= 0) {
+        console.log("Timmer has expired");
+        process.exit();
+    }
+    const min = Math.floor((timeDiff % (3600 * 24)) / 3600)
+    const sec = Math.floor(timeDiff % 60);
+    console.log(`${min.toString().padStart(1, "0")}:${sec.toString().padStart(1, "0")}`);
+  }),1000);
+}
+
+startTime(input);
+
+ 
